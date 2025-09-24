32fa427560a90611a7d9179bbffcfd1c5c40d33f
Carlou134 <carlouvasquez134@gmail.com>
2025-09-24 01:26:17 -0500

    Nuevos modulos

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+  name = "Carlos";
+  role = "Fullstack Developer";
+  avatar = "https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png";
+
+  
+}
