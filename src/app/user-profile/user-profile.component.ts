d138b9d56e7dff0bff1c06f2495d5b3aa3eab78a
Carlou134 <carlouvasquez134@gmail.com>
2025-09-24 01:43:37 -0500

    Parte 1 de Angular

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,9 +1,10 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
@@ -11,6 +12,4 @@ export class UserProfileComponent {
   name = "Carlos";
   role = "Fullstack Developer";
   avatar = "https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png";
-
-  
 }
